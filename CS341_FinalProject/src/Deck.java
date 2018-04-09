36a1aa90b94f4c278ab289835105ff706b30e489
Tristin <34673748+Vithiar@users.noreply.github.com>
2018-04-09 00:00:18 -0500

    Framework
    This contains most of the files we should need.

---
+++ CS341_FinalProject/src/Deck.java
@@ -0,0 +1,26 @@
+
+/**
+ * Course: CS341 Data Structures
+ * Date: April 2018
+ * Assignment: CS341_FinalProject
+ * Authors: Trevor Conway, Tristin Harvell, Travis Kruse, Johnny Tran
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+
+    /*
+
+    private static List<Card> makeDeck() {
+        List<Card> deckOfCards = new ArrayList<>(52);
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Value value : Card.Value.values()) {
+                deckOfCards.add(new Card(value, suit));
+            }
+        }
+        return deckOfCards;
+    }
+
+     */
+}
---
+++ CS341_FinalProject/src/DeckInterface.java
@@ -0,0 +1,16 @@
+
+/**
+ * Course: CS341 Data Structures
+ * Date: April 2018
+ * Assignment: CS341_FinalProject
+ * Authors: Trevor Conway, Tristin Harvell, Travis Kruse, Johnny Tran
+ */
+public class DeckInterface {
+
+    //java collections has a shuffle method?
+    public void shuffle() {
+    }
+;
+
+//size and pop? stack?
+}
